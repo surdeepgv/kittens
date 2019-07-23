3ad651b8d116942f78cbfdaad6ab03fc1f9eda87
Surdeep Govindaraj <surdeepgv@gmail.com>
2019-07-23 16:58:59 +0530

    Updates

---
+++ precache-manifest.0756917b91bf393e12743a4e44603207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab92d4f258f6b5d79eb1f3580eed9115",
+    "url": "/kittens/index.html"
+  },
+  {
+    "revision": "1648c7d60b2c8aef03c6",
+    "url": "/kittens/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c83ba206b9ab4a7a716d",
+    "url": "/kittens/static/css/main.e3d8c79f.chunk.css"
+  },
+  {
+    "revision": "1648c7d60b2c8aef03c6",
+    "url": "/kittens/static/js/2.fda027d4.chunk.js"
+  },
+  {
+    "revision": "c83ba206b9ab4a7a716d",
+    "url": "/kittens/static/js/main.aee56c4e.chunk.js"
+  },
+  {
+    "revision": "9746056b9c9c4f8a6b6f",
+    "url": "/kittens/static/js/runtime~main.beba78c1.js"
+  },
+  {
+    "revision": "cccc18d85c0a444a64aa9feef42c5fa9",
+    "url": "/kittens/static/media/Kitty face.cccc18d8.woff"
+  }
+]);
